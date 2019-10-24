7596738f2df9360623f316d8266f0acd2d2885da
Waliston <walistonbelles@bol.com.br>
2019-10-23 21:17:29 -0300

    Novo metodo

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var createError = require('http-errors');
 var express = require('express');
-var consign = require('consign');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var session = require('express-session');
+
+//inclui as rotas
+var routes = require('./routes');
 
 // instancia um servidor
 var app = express();
 
 // instancia as sessões (secret é do express)
-app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+app.use(session({secret: 'alguma senha',saveUninitialized: true,resave: true}));
 
 // informa o diretório de views padrão (considera o path a partir de app.js)
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-// body parser (midleware: requisição/resposta).. extended = formato json
+// extended = formato json
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,25 +29,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 // middleware de validação
 app.use(expressValidator());
 
-// inclui o diretório routes no consign (autoload da aplicação)
-consign()
-  .include('routes')
-  .into(app);
+// SITE
+app.get('/', routes.index);
+app.get('/login', routes.login);
+app.get('/servicos', routes.servicos);
+app.get('/sobre', routes.sobre);
+app.get('/contatos', routes.contatos);
+app.post('/entrar', routes.entrar);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// ADMIN
+app.get('/admin', routes.admin);
+
+// iniciar um servidor com uma porta específica
+app.listen(3000, function () {
+  console.log('Servidor executando na porta 3000..');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,76 @@
+// conexão com o postgres
+const { Client } = require('pg');
+//Cria a string de conexão com o PostgreSQL.
+var connectionString = 'postgress://postgres@localhost/Endless';
+//Abre a conexão com o banco de dados.
+const client = new Client({ connectionString: connectionString }); 
+client.connect();
 
-// ROTAS EM EXPRESS E RENDERIZAÇÃO COM O EJS
+// rota index 
+exports.index = function (req, res) {
+	res.render('index');
+};
 
-// recebe de app.js
-module.exports = function(app) {
-  // callback: req, res
-  app.get('/', function(req, res) {
-    res.render('index');
-  });
+// rota login
+exports.login = function(req, res) {
+    res.render('login', { validacao: {}, acesso: {} });
+ };
+
+// rota serviços
+exports.servicos = function(req, res) {
+
+    client.query('SELECT * FROM servicos', function (err, result) {
+	      if (err) {
+	          console.log(err);
+	          res.status(400).send(err);
+	      }
+	      res.render('servicos', { servicos: result.rows });
+	  });
+ };
+
+// rota sobre
+exports.sobre = function (req, res) {
+	res.render('sobre');
+};
+
+// rota sobre
+exports.contatos = function (req, res) {
+	res.render('contatos');
+};
+
+//rota admin
+exports.admin = function (req, res) {
+	res.render('admin');
+};
+
+// rota entrar
+exports.entrar= function (req, res) { 
+  var acesso = req.body; 
+  // console.log(acesso);
+  req.assert('email', 'O e-mail é obrigatório').notEmpty();
+  req.assert('senha', 'A senha é obrigatória').notEmpty();
+  req.assert('senha', 'O senha deve conter entre 5 e 10 caracteres').len(5, 10); 
+  var erros = req.validationErrors();
+  //console.log(erros);
+  // se o formulário não foi validado, envia para a tela de login novamente
+  if (erros) {
+    res.render('login', { validacao: erros, acesso: acesso, sess: sess });
+    return;
+  }
+  // verifica a senha de acesso
+  if(req.body.senha == '12345') {
+    // recebe o formulário da requisição
+    sess = req.session;
+    // atribui o name "email" do formulário à sessão
+    sess.email = req.body.email;
+    // redireciona para a rota da área administrativa
+    res.redirect('/admin');
+  }
+  // caso a senha esteja incorreta, envia para a tela de login novamente
+  else{
+    // A FAZER: ENVIAR UMA MSG DE VALIDAÇÃO
+    res.render('login', { validacao: {}, acesso: {}, sess: sess });
+  return;
+  }
+
 };
